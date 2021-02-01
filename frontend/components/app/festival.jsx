36b3856d2f199e08928d606d6260aed5be806a18
saif aziz <saifaziz08@gmail.com>
2021-02-01 02:38:28 -0500

    page

--- frontend/components/app/festival.jsx
+++ frontend/components/app/festival.jsx
@@ -35,40 +35,76 @@ class Festival extends React.Component {
                 <Nav page="my-list" onList={true} />
                 <Nav page="moviefestival" onList={true} />
                 <Nav page="pricing" onList={true} />
-
                 <div className="list-container">
                     <div className="movie-festival-container">
-                        <div className="content-1">
-                                <div className="custom-headings">Karawood History</div>
+                        
+                <div className="content-1">
+                            
+                                <div className="content-images"> 
+                                 <div  className="banner-content" >Welcome <br/>To The<br/> Festival  </div></div>
+                                <div className="custom-headings">History</div>
+                                
                                 <div className="content-2">
                                      Karawood Film Festival is a festival dedicated to international short films of all genres, aiming to find new voices and perspectives to celebrate the power of a good story. We are committed to bring the best of worldwide cinema.Once a year, the Best Short of the Year will be announced and will receive a USD 3500 cash prize. No other city in Europe honors the cinema as Cannes does, living and breathing throughout the year under the lights of cinema. It is a unique and picturesque historic location along the French Riviera where you can experience the entirety of the fantastic film world. Since 1939, it has been “the” city of cinema, where you can find as much talent as the stars that it welcomes and without any doubt is the number-one international market for filmmakers. We are willing to screen your film in different markets amongst diverse audiences, in order to support you in developing your uprising career 
                                 </div>
+
                                 <div className="custom-headings">
                                      Rules & Terms
                                 </div>
-                                <div className="content-2">
-                                1- In order to submit a film, you must own the rights or have permission from the owner. <br/>
-                                2- The Festival relationship will exclusively be related to the film submitter.<br/>
-                                3- Any Foreign film that is not in English must have English subtitles.<br/>
-                                4- All fees are non-refundable.<br/>
-                                5- Kawarood will not be responsible for damaged copies.<br/>
-                                6- Students must present a valid Student ID in order to receive our student discount.<br/>
-                                7- Not all the submissions are selected for the official competition. Our Judges are free to make their own choices and no answers will be given about not selected films.<br/>
-                                8- Kawarood can consider any film to be disqualified if the festival considers the content inappropriate.<br/>
-                                9- Filmmakers can submit the film in multiple categories in order to become a multi-award film.<br/>
-                                     Our Team may suggest the categories that are more suitable for the film to apply. Still, the decision about the selection is strictly determined by our judges' team and no selection is guaranteed.<br/>
-                                10- A fee is required for the main category submission and also fees for additional categories. For payment structure visit (FilmFreeway website).<br/>
-                                11- The applicants (submitters) must accept Kawarood Rules and Terms.<br/>
-                                12- All submissions in personal categories required the name of the person and the category. You can easily send us this information in the cover letter at FilmFreeway website.<br/>
-                                13- Submitting your movie, you authorize Kawarood to use, distribute and share the film poster, trailer, synopsis and movie stills in our social media.<br/>
-                                14- Maximum length allowed is 40 minutes.<br/>
-                                15- The annual prize of usd 3500 will be given once a year to the Best Short of the Year.<br/>
-                                </div> 
                                 
+                                <div className="content-3">
+                                    <ol>
+                                        <li>
+                                             In order to submit a film, you must own the rights or have permission from the owner.
+                                        </li>
+                                        <li>
+                                             The Festival relationship will exclusively be related to the film submitter
+                                        </li>
+                                        <li>
+                                          Any Foreign film that is not in English must have English subtitles
+                                        </li>
+                                        <li>
+                                         All fees are non-refundable
+                                        </li>
+                                        <li>
+                                          Kawarood will not be responsible for damaged copies
+                                        </li>
+                                        <li>
+                                         Students must present a valid Student ID in order to receive our student discount
+                                        </li>
+                                        <li>
+                                          Not all the submissions are selected for the official competition. Our Judges are free to make their own choices and no answers will be given about not selected films
+                                        </li>
+                                        <li>
+                                        Filmmakers can submit the film in multiple categories in order to become a multi-award film
+                                         </li>
+                                        <li>
+                                         Our Team may suggest the categories that are more suitable for the film to apply. Still, the decision about the selection is strictly determined by our judges' team and no selection is guaranteed.
+                                        </li>
+                                        <li>
+                                         A fee is required for the main category submission and also fees for additional categories. For payment structure visit (FilmFreeway website)
+                                        </li>
+                                        <li>
+                                         All submissions in personal categories required the name of the person and the category. You can easily send us this information in the cover letter at FilmFreeway website
+                                        </li>
+                                        <li>
+                                         Submitting your movie, you authorize Kawarood to use, distribute and share the film poster, trailer, synopsis and movie stills in our social media
+                                        </li>
+                                        <li>
+                                         Maximum length allowed is 40 minutes
+                                        </li>
+                                        <li>
+                                         The annual prize of usd 3500 will be given once a year to the Best Short of the Year
+                                        </li>  
+                                    </ol> 
+                                </div> 
+                                <a href="#" className="mybutton">Submit</a>
                                 <div className="custom-headings">
                                     Awards & Prizes
                                 </div>
+
                                 <div className="content-2">
+
                                 <button className="accordion">Best Short</button>
                                     <div className="panel">
                                         <p>Lorem ipsum...</p>
